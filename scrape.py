135a7ec9a994d12620fcc4bfad1f571db1cdd810
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-15 08:06:25 -0400

    Added notebook

--- scrape.py
+++ scrape.py
@@ -4,56 +4,35 @@ import re
 import requests
 from pprint import pprint
 
+
 class Scrape():
+ 
+    def scrapeHTML(url):
 
-    '''
-        this method takes a url, scrapes the html and stores it in the database as 
-        a document.  It also appends the url into the "done" collection so that 
-        the system can check it before calling the method.  It also calls the 
-        scrapeURL to extract all the urls from the page
-    '''
-    def scrapeHTML(url, htmlCollection, done_LinksCollection, todo_LinksCollection):
+        # get url
+        try:   
+            response = requests.get(url)
+        except Exception as e:
+            print(f'ScrapeHTML: {e}')
 
-        # get html
-        with requests.get(url) as response:
-            html = response.text
+            # return false for scrapeURL check in parent app
+            return False
 
-        # define dict to insert into db
-        data = {
-            "url": url, 
-            "html": html, 
-            }
-
-        # insert dict into db document
-        htmlCollection.insert_one(data)
-
-        # record url in "done" collection
-        urlData = {
-            "url": url
-        }
-        done_LinksCollection.insert_one(urlData)
-
-        # call the scrapeURL method to capture all the links on the page
-        # Scrape.scrapeURL(html, todo_LinksCollection)
-
-        return html
+        return response.text
     
-    def scrapeURL(html, todo_LinksCollection):
+    def scrapeURL(html, startURL):
         # create soup object to parse
         soup = bs(html, 'lxml')
 
-        # pull Set object from todo_LinksCollection
-        links = Set([])     # NOT RIGHT
+
 
         # crawl through soup object, find links, add to set object
-        for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-            links.add(link.get('href'))
+        links = set()
+        for link in soup.findAll('a'):
+            if 'http' in startURL:
+                links.add(urllib.parse.urljoin(startURL, link.get('href')))
+            else:
+                continue
 
-        # TODO: save Set object to database
-
-        # test
-        pprint(links)
-
-        
-
+        return links
 
