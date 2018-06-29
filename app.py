54269a2ca313a82c50cf4e090a1c14b512b40665
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-29 19:54:02 -0400

    Added favicon

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import (Flask, jsonify, render_template, 
-                   send_file, url_for)
+                   send_file, url_for, send_from_directory)
 import json
+import os
 
 
 #make an app instance
 app = Flask(__name__)
 
 '''Define the Routes'''
+
+#favicon
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/img/'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
 
 #index
 @app.route('/')
